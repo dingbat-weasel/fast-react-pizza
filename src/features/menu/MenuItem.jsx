9c4c8b100394b0b6f1e20feac850a9e359d97f10
Isaac Perk <97003977+isaacperk@users.noreply.github.com>
2023-09-08 12:23:23 -0700

    updating pizza quantity in menu and cart

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,9 @@
 import { useDispatch, useSelector } from "react-redux";
+import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 
 import Button from "../../ui/Button";
-import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantity";
 
 import { formatCurrency } from "../../utils/helpers";
 
@@ -11,7 +12,6 @@ function MenuItem({ pizza }) {
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   const currentQuantity = useSelector(getCurrentQuantityById(id));
-  console.log(currentQuantity);
   const isInCart = currentQuantity > 0;
 
   function handleAddToCart() {
@@ -46,7 +46,15 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {isInCart && <DeleteItem pizzaId={id} />}
+          {isInCart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
 
           {!soldOut && !isInCart && (
             <Button type="small" onClick={handleAddToCart}>
